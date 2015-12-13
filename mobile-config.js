b5cc86db53d55916f76443a712109e897621acef
subramaniashiva <vengaishiva@gmail.com>
2015-12-14 00:27:26 +0530

    Updated mobile config with app icons and splash screens

--- mobile-config.js
+++ mobile-config.js
@@ -2,8 +2,8 @@
 // the entire section is optional.
 App.info({
   id: 'com.example.matt.uber',
-  name: 'über',
-  description: 'Get über power in one button click',
+  name: 'nisaptham',
+  description: 'Read the blog contents of nisaptham.com',
   author: 'Siva',
   email: 'contact@example.com',
   website: 'http://example.com'
@@ -11,17 +11,25 @@ App.info({
 App.accessRule("*");
 
 // Set up resources such as icons and launch screens.
-/*App.icons({
-  'iphone': 'icons/icon-60.png',
-  'iphone_2x': 'icons/icon-60@2x.png',
-  // ... more screen sizes and platforms ...
+App.icons({
+  //'android_ldpi': 'resources/icons/ic_launcher_36x36.png',
+  'android_mdpi': 'resources/icons/ic_launcher_48x48.png',
+  'android_hdpi': 'resources/icons/ic_launcher_72x72.png',
+  'android_xhdpi': 'resources/icons/ic_launcher_96x96.png',
+  //'android_xxhdpi': 'resources/icons/ic_launcher_144x144.png',
+  //'android_xxxhdpi': 'resources/icons/ic_launcher_192x192.png'
 });
 
 App.launchScreens({
-  'iphone': 'splash/Default~iphone.png',
-  'iphone_2x': 'splash/Default@2x~iphone.png',
-  // ... more screen sizes and platforms ...
-});*/
+  //'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  //'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash_portraite_320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash_landscape_480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash_portraite_480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash_landscape_800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash_portraite_720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash_landscape_1280x720.png'
+});
 
 // Set PhoneGap/Cordova preferences
 //App.setPreference('BackgroundColor', '0xff0000ff');
