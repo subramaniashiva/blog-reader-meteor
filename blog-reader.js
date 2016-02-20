f4e25880b159d6ad6d82f8bba446b4c337b3a18f
subramaniashiva <vengaishiva@gmail.com>
2016-02-20 23:52:05 +0100

    Code optimization. Changed the side menu. Moved the config to separate file

--- blog-reader.js
+++ blog-reader.js
@@ -1,55 +1,79 @@
+// This object holds all the blog posts.
 Posts = new Mongo.Collection(null);
 if (Meteor.isClient) {
-  var bloggerAPI = '';
-  var labelsAPI = '';
+  // Application config loaded from lib/constants.js
+  var config = CONSTANTS;
+  // Function to create a blogger API call URL.
+  var makeBloggerUrl = function(config, options) {
+    return config.bloggerAPI + config.blogs[options.currentBlogIndex] + 
+      '/posts?key=' + config.apiKey + '&maxResults=' + config.resultsPerPage;
+  }
+  // Get the current blog index to be crawled. Defaults to 0
+  var currentBlogIndex = config.defaultBlogIndex;
+  // Get the current blog id to be crawled. Default is nisaptham.com blog id
+  var currentBlogId = config.blogs[currentBlogIndex];
+
+  // Create the blooger API Url
+  var bloggerAPI = makeBloggerUrl(config, {currentBlogIndex: currentBlogIndex});
+  // Create the API Url for crawling labels
+  var labelsAPI = bloggerAPI + '&labels=';
+  // Store the current labels. Used in the side menu
+  var labels = config.labels[currentBlogId];
+  // Set the current label to the first label
+  var currrentLabel = labels[0];
+  // Hold the loading screen till the posts gets loaded. (Useful only in mobile apps)
   var handle = LaunchScreen.hold();
   var nextPageToken = '';
-  var currrentLabel = 'முகப்பு';
-  var labels = ['முகப்பு','அறக்கட்டளை', 'பத்தி', 'புனைவு', 'நூல்முகம்', 'கடிதங்கள்'];
+  // Slide out menu instance. Will be reused across functions to open/close side menu
+  var slideoutInstance;
+  // Helper function to get the blog posts by URL
+  // options is an object. The only key that it has now is 'clearExistingPosts'. 
+  // Set it to true when changing the labels.
+  var getBlogPosts = function(url, options) {
+    $('#loading').show();
+    $.ajax({
+      url: url,
+      dataType: 'json'
+    })
+    .done((response) => {
+      response = JSON.parse(JSON.stringify(response));
 
-  function initiateAJAX(url, callback) {
-      var xmlhttp = new XMLHttpRequest();
-      $('#loading').show();
-      xmlhttp.onreadystatechange = function() {
-          if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-              if (xmlhttp.status == 200) {
-                callback(xmlhttp.responseText);
-              } else if (xmlhttp.status == 400) {
-                console.log('There was an error 400');
-                handle.release();
-                $('#loading').hide();
-              } else {
-                console.log('something else other than 200 was returned');
-                handle.release();
-                $('#loading').hide();
-              }
-          }
-      }
-
-      xmlhttp.open("GET", url, true);
-      xmlhttp.send();
-  }
-
-  function callback(response, clearArray) {
-      response = JSON.parse(response);
-      var outputHtml = '',
-          templateHtml = '',
-          tempObj;
-      if(clearArray) {
+      if(options && options.clearExistingPosts) {
         Posts.remove({});
       }
-      for (var i = 0; i < response.items.length && i < 20; i++) {
-          Posts.insert({
-              title: response.items[i].title,
-              content: response.items[i].content
-          });
-      }
+
+      response.items.map((item) => {
+        Posts.insert({
+            title: item.title,
+            content: item.content
+        });
+      });
+
       nextPageToken = response.nextPageToken;
+    })
+    .always(() => {
       handle.release();
       $('#loading').hide();
+      slideoutInstance.close();
+    });
   }
-
-  Template.body.helpers({
+  // When the master layout is rendered, instantiate the side menu
+  Template.MasterLayout.onRendered(function () {
+    var template = this;
+    slideoutInstance = new Slideout({
+      'menu': template.find('.menu'),
+      'panel': template.find('.panel'),
+      'padding': 256,
+      'tolerance': 70
+    });
+  });
+  // On clicking on the side menu button, the menu toggles
+  Template.MasterLayout.events({
+    "click #toggle": function(e) {
+      slideoutInstance.toggle();
+    }
+  });
+  Template.MasterLayout.helpers({
     posts: function() {
         return Posts.find({});
     }   
@@ -65,13 +89,9 @@ if (Meteor.isClient) {
       if(selectedLabel !== currrentLabel) {
         currrentLabel = selectedLabel;
         if(selectedLabel === 'முகப்பு') {
-          initiateAJAX(bloggerAPI, function(response) {
-            callback(response, true);
-          });
+          getBlogPosts(bloggerAPI, {clearExistingPosts: true});
         } else {
-          initiateAJAX(labelsAPI + selectedLabel, function(response) {
-            callback(response, true)
-          });
+          getBlogPosts(labelsAPI + selectedLabel, {clearExistingPosts: true});
         }
       }
     }
@@ -86,9 +106,9 @@ if (Meteor.isClient) {
   Template.loadMore.events({
     "click #loadPosts": function(e) {
       if(nextPageToken) {
-        initiateAJAX(bloggerAPI+'&pageToken='+nextPageToken, callback);
+        getBlogPosts(bloggerAPI+'&pageToken='+nextPageToken);
       } else {
-        initiateAJAX(bloggerAPI, callback);
+        getBlogPosts(bloggerAPI);
       }
     }
   });
@@ -96,17 +116,13 @@ if (Meteor.isClient) {
   Template.refreshPosts.events({
     "click #refreshPosts": function(e) {
       if(currrentLabel === 'முகப்பு') {
-        initiateAJAX(bloggerAPI, function(response) {
-          callback(response, true);
-        });
+        getBlogPosts(bloggerAPI, {clearExistingPosts: true});
       } else {
-        initiateAJAX(labelsAPI + currrentLabel, function(response) {
-          callback(response, true);
-        });
+        getBlogPosts(labelsAPI + currrentLabel, {clearExistingPosts: true});
       }
     }
   });
-  initiateAJAX(bloggerAPI, callback);
+  getBlogPosts(bloggerAPI);
 }
 
 if (Meteor.isServer) {
